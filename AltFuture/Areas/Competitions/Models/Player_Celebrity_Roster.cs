b43e82c3d280424d9225f094e17dd3400db28201
jgogola <jason@gogola.us>
2023-01-10 18:34:23 -0500

    Player Celebrity Roster Model Update
    .

--- AltFuture/Areas/Competitions/Models/Player_Celebrity_Roster.cs
+++ AltFuture/Areas/Competitions/Models/Player_Celebrity_Roster.cs
@@ -1,12 +1,27 @@
-﻿namespace AltFuture.Areas.Competitions.Models
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace AltFuture.Areas.Competitions.Models
 {
     public class Player_Celebrity_Roster
     {
         public int player_celebrity_roster_key { get; set; } = 0;
-        public int competition_key { get; set; } = 0;
+
+        [Required]
+        [Display(Name ="Player")]
         public int competition_player_key { get; set; } = 0;
+
+        [Required]
+        [Display(Name ="Celebrity")]
         public int celebrity_key { get; set; } = 0;
         public Boolean is_winner { get; set; } = false;
+
+        [ValidateNever]
+        public Competition_Player competition_Player { get; set; } = new Competition_Player();
+
+        [ValidateNever]
+        public Celebrity celebrity { get; set; }   = new Celebrity();
+
 
     }
 }
